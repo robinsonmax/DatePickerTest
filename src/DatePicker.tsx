da67701393ed50f6326578db39612c738fd44e72
Max Robinson <robinsonmax17@gmail.com>
2023-05-07 12:46:47 +0100

    🥳 init

---
+++ src/DatePicker.tsx
@@ -0,0 +1,11 @@
+import styles from "./DatePicker.module.scss";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <p>Date Picker</p>
+    </div>
+  );
+}
+
+export default App;
