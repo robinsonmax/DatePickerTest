fbe798b235c18858ab50b23c7bb96c69b14172ee
Max Robinson <robinsonmax17@gmail.com>
2023-05-07 13:27:22 +0100

    🔁 added a transition between date picker & time picker sections

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,11 +1,71 @@
+import { useState } from "react";
 import styles from "./DatePicker.module.scss";
 
-function App() {
+enum States {
+  Date,
+  Time,
+}
+
+export default function DatePicker() {
+  const [state, setState] = useState<States>(States.Date);
+
   return (
     <div className={styles.container}>
-      <p>Date Picker</p>
+      <div
+        className={`${styles.sectionContainer} ${
+          state === States.Date ? styles.expand : ""
+        }`}
+      >
+        <div className={styles.section}>
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+          <br />
+          Date Picker
+        </div>
+      </div>
+      <div className={styles.switch}>
+        <button
+          onClick={() => {
+            setState((state) =>
+              state === States.Date ? States.Time : States.Date
+            );
+          }}
+        >
+          Toggle
+        </button>
+      </div>
+      <div
+        className={`${styles.sectionContainer} ${
+          state === States.Time ? styles.expand : ""
+        }`}
+      >
+        <div className={styles.section}>
+          Time Picker
+          <br />
+          Time Picker
+          <br />
+          Time Picker
+          <br />
+          Time Picker
+          <br />
+          Time Picker
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
